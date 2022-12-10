06992f2e4882216f78912fff62b273a57e45d356
Arkadiusz Wolk <awolk@student.agh.edu.pl>
2022-12-10 19:21:39 +0100

    DI | refactor

--- Framework/DataInspector/src/DataInspector.h
+++ Framework/DataInspector/src/DataInspector.h
@@ -22,23 +22,6 @@
 
 namespace o2::framework::DataInspector
 {
-/* Checks if a command line argument relates to the data inspector. */
-inline bool isInspectorArgument(const char* argument)
-{
-  return std::strcmp(argument, "--inspector") == 0;
-}
-
-/* Checks if device is used by the data inspector */
-inline bool isInspectorDevice(const DataProcessorSpec& spec)
-{
-  return spec.name == "DataInspector";
-}
-
-inline bool isInspectorDevice(const DeviceSpec& spec)
-{
-  return spec.name == "DataInspector";
-}
-
 inline bool isNonInternalDevice(const DeviceSpec& spec)
 {
   return spec.name.find("internal") == std::string::npos;
